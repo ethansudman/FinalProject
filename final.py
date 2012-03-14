2e1d166ab04b656425209024280e72f7daa227f5
ethansudman <ethan.sudman@comcast.net>
2012-03-14 01:12:52 -0700

    Changes

--- final.py
+++ final.py
@@ -1,1 +1,14 @@
 from psim import PSim
+
+def dijkstra(V, E, w, s):
+	Vt = s
+	# Loop through all of the vertices but the starting/ending one
+	for vte in filter(lambda x: not x == Vt, V):
+		if (s, vte) in w:
+			l[vte] = w[(s, vte)]
+		else:
+			l[vte] = float("inf")
+
+	#while not Vt =
+
+
