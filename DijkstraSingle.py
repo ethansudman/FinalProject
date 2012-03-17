0499a252c5084c0902cbee5192862c584dad7fa4
ethansudman <ethan.sudman@comcast.net>
2012-03-16 22:02:39 -0700

    Refactoring

--- DijkstraSingle.py
+++ DijkstraSingle.py
@@ -2,25 +2,25 @@ from priorityDict import priorityDictionary
 from psim import PSim
 
 def Dijkstra(G,start,end=None):
-	D = {}	# dictionary of final distances
-	P = {}	# dictionary of predecessors
+	Distances = {}
+	Predecessors = {}	# dictionary of predecessors
 	Q = priorityDictionary()   # est.dist. of non-final vert.
 	Q[start] = 0
 	
 	for v in Q:
-		D[v] = Q[v]
+		Distances[v] = Q[v]
 		if v == end: break
 		
 		for w in G[v]:
-			vwLength = D[v] + G[v][w]
-			if w in D:
-				if vwLength < D[w]:
-					raise ValueError, "Dijkstra: found better path to already-final vertex"
-			elif w not in Q or vwLength < Q[w]:
-				Q[w] = vwLength
-				P[w] = v
+			length = Distances[v] + G[v][w]
+			if w in Distances:
+				if length < Distances[w]:
+					raise ValueError, "Found a better path to a final vertex"
+			elif w not in Q or length < Q[w]:
+				Q[w] = length
+				Predecessors[w] = v
 	
-	return (D,P)
+	return (Distances,Predecessors)
 
 """
 I haven't quite been able to get this working, but the idea is to parallelize the
